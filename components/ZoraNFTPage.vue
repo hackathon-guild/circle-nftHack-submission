c1e20fb199b68efcad56eaf7ba63319129dcd49b
dhruvmalik007 <dhruv.malik@polytechnique.edu>
2022-01-15 17:16:30 +0100

    adding ethers for wallet management in backend

--- components/ZoraNFTPage.vue
+++ components/ZoraNFTPage.vue
@@ -1,12 +1,17 @@
 <template>
-<v-layout>
+  <v-layout>
     <v-flex>
-        <v-card> </v-card>
+      <v-card>
+        <form id="NFT_Wallet" @submit="checkForm"></form>
+      </v-card>
     </v-flex>
-</v-layout>
+  </v-layout>
 </template>
 
 <script>
+import {constructMediaData , sha256FromBuffer, generateMetadata} from '@zoralabs/zdk'
+import {Signer , Wallet } from 'ethers'
+
 export default {}
 </script>
 
